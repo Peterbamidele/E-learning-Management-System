88cc43fd7e8e2e201b560eb8e0b62b86a5d97783
Peterbamidele <peterbamidele@gmail.com>
2021-09-17 13:23:54 +0100

    project setup

---
+++ src/main/java/com/elearningmangementsystem/ELearningMangementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elearningmangementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningMangementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningMangementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elearningmangementsystem/ELearningMangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearningmangementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningMangementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

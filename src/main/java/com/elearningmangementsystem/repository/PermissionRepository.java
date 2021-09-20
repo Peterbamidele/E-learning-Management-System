11931c70c82102d7bc605fdcea1b4a3805ecd51a
Peterbamidele <peterbamidele@gmail.com>
2021-09-20 03:55:33 +0100

    repository and service added

--- src/main/java/com/elearningmangementsystem/repository/PermissionRepository.java
+++ src/main/java/com/elearningmangementsystem/repository/PermissionRepository.java
@@ -2,6 +2,8 @@ package com.elearningmangementsystem.repository;
 
 import com.elearningmangementsystem.model.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Integer> {
 }
---
+++ src/main/java/com/elearningmangementsystem/service/UserService.java
@@ -0,0 +1,14 @@
+package com.elearningmangementsystem.service;
+
+import com.elearningmangementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
